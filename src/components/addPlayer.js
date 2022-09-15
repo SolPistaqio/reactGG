6388bd551104df851cb261f6cdbc04f89a53ee6c
Solvita <solvita.dvortsova@henleyglobal.com>
2022-09-15 11:55:18 +0300

    add button

--- src/components/addPlayer.js
+++ src/components/addPlayer.js
@@ -3,6 +3,9 @@ import { Icon } from "@iconify/react";
 import "./css/addButton.css";
 
 class AddGhost extends Component {
+  state = {
+    color: "#fec302",
+  };
   openMarket(e) {
     console.log("open it!");
   }
@@ -12,7 +15,10 @@ class AddGhost extends Component {
         icon="carbon:add-filled"
         height="100px"
         className="add"
+        color={this.state.color}
         onClick={() => this.openMarket()}
+        onMouseOver={() => this.setState({ color: "#1919ae" })}
+        onMouseLeave={() => this.setState({ color: "#fec302" })}
       />
     );
   }
